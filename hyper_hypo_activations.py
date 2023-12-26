8189c7978d8ec8ccee71c9185150d9df68dfe242
Nikolai Gruzinov <ngruzinov@constructor.university>
2023-12-26 17:16:13 +0100

    ruff format

--- hyper_hypo_activations.py
+++ hyper_hypo_activations.py
@@ -11,30 +11,29 @@ import einops
 import os
 import argparse
 
+
 def tokenize_prompts(prompts: list[str]) -> tuple[torch.Tensor, torch.Tensor]:
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-13b-hf")
-    tokenizer.padding_side = 'right'
+    tokenizer.padding_side = "right"
     tokenizer.pad_token = tokenizer.eos_token
 
     encode_res = tokenizer.batch_encode_plus(
         prompts,
-        return_tensors='pt',
+        return_tensors="pt",
         padding=True,
         add_special_tokens=True,
-        return_attention_mask=True
+        return_attention_mask=True,
     )
 
-    token_ids = encode_res['input_ids']
-    attention_mask = encode_res['attention_mask']
+    token_ids = encode_res["input_ids"]
+    attention_mask = encode_res["attention_mask"]
 
     return token_ids, attention_mask
 
 
 def load_model(device="cuda"):
     model = LlamaForCausalLM.from_pretrained(
-        f"meta-llama/Llama-2-13b-hf",
-        low_cpu_mem_usage=True,
-        torch_dtype=torch.float16
+        f"meta-llama/Llama-2-13b-hf", low_cpu_mem_usage=True, torch_dtype=torch.float16
     )
     model.eval()
     print("Moving model to CUDA")
@@ -43,8 +42,9 @@ def load_model(device="cuda"):
 
 
 @torch.no_grad()
-def get_layer_activations_hf(bs: int, tokenized_prompts: torch.Tensor,
-                             attention_mask: torch.Tensor, device=None):
+def get_layer_activations_hf(
+    bs: int, tokenized_prompts: torch.Tensor, attention_mask: torch.Tensor, device=None
+):
     model = load_model()
 
     layers = list(range(model.config.num_hidden_layers))
@@ -65,14 +65,20 @@ def get_layer_activations_hf(bs: int, tokenized_prompts: torch.Tensor,
     print("Starting the Dataloader loop over batches for Llama")
     for step, batch in enumerate(tqdm(dataloader, disable=False)):
         # clip batch to remove excess padding
-        batch_attention_mask = attention_mask[step*bs:(step+1)*bs]
-        last_valid_ix = torch.argmax(
-            (batch_attention_mask.sum(dim=0) > 0) * torch.arange(ctx_len)) + 1
+        batch_attention_mask = attention_mask[step * bs : (step + 1) * bs]
+        last_valid_ix = (
+            torch.argmax((batch_attention_mask.sum(dim=0) > 0) * torch.arange(ctx_len)) + 1
+        )
         batch = batch[:, :last_valid_ix].to(device)
         batch_attention_mask = batch_attention_mask[:, :last_valid_ix]
 
-        out = model(batch.to("cuda"), output_hidden_states=True,
-                    output_attentions=False, return_dict=True, use_cache=False)
+        out = model(
+            batch.to("cuda"),
+            output_hidden_states=True,
+            output_attentions=False,
+            return_dict=True,
+            use_cache=False,
+        )
         print("Got first layer activation")
 
         # do not save post embedding layer activations
@@ -83,8 +89,7 @@ def get_layer_activations_hf(bs: int, tokenized_prompts: torch.Tensor,
             processed_activations = process_activation_batch(activation, step, batch_attention_mask)
 
             save_rows = processed_activations.shape[0]
-            layer_activations[lix][offset:offset +
-                                   save_rows] = processed_activations
+            layer_activations[lix][offset : offset + save_rows] = processed_activations
 
         offset += batch.shape[0]
 
@@ -99,9 +104,7 @@ def process_activation_batch(batch_activations, step, batch_mask=None):
     d_act = batch_activations.shape[2]
     expanded_mask = last_entity_token.unsqueeze(-1).expand(-1, d_act)
     processed_activations = batch_activations[
-        torch.arange(cur_batch_size).unsqueeze(-1),
-        expanded_mask,
-        torch.arange(d_act)
+        torch.arange(cur_batch_size).unsqueeze(-1), expanded_mask, torch.arange(d_act)
     ]
     assert processed_activations.shape == (cur_batch_size, d_act)
     return processed_activations
@@ -112,11 +115,11 @@ def quantize(activation_tensor: torch.Tensor) -> torch.Tensor:
     max_vals = activation_tensor.max(dim=0)[0]
 
     output_precision = 8
-    num_quant_levels = 2 ** output_precision
+    num_quant_levels = 2**output_precision
 
     scale = (max_vals - min_vals) / (num_quant_levels - 1)
     zero_point = torch.round(-min_vals / scale)
-    return torch.quantize_per_channel(activation_tensor, scale, zero_point, 1,  torch.quint8)
+    return torch.quantize_per_channel(activation_tensor, scale, zero_point, 1, torch.quint8)
 
 
 def write_activations_to_disk(layer_activations: dict[int, torch.Tensor], save_dir: str):
