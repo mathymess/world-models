53642912378e3d94f4d7175bee3da49ad09ad4a8
Nikolai Gruzinov <ngruzinov@constructor.university>
2023-12-26 17:15:29 +0100

    Fix bugs to run, add simple logging

--- hyper_hypo_activations.py
+++ hyper_hypo_activations.py
@@ -37,7 +37,8 @@ def load_model(device="cuda"):
         torch_dtype=torch.float16
     )
     model.eval()
-    model.to()
+    print("Moving model to CUDA")
+    model.to("cuda")
     return model
 
 
@@ -61,6 +62,7 @@ def get_layer_activations_hf(bs: int, tokenized_prompts: torch.Tensor,
     offset = 0
     dataloader = DataLoader(tokenized_prompts, batch_size=bs, shuffle=False)
 
+    print("Starting the Dataloader loop over batches for Llama")
     for step, batch in enumerate(tqdm(dataloader, disable=False)):
         # clip batch to remove excess padding
         batch_attention_mask = attention_mask[step*bs:(step+1)*bs]
@@ -69,8 +71,9 @@ def get_layer_activations_hf(bs: int, tokenized_prompts: torch.Tensor,
         batch = batch[:, :last_valid_ix].to(device)
         batch_attention_mask = batch_attention_mask[:, :last_valid_ix]
 
-        out = model(batch, output_hidden_states=True,
+        out = model(batch.to("cuda"), output_hidden_states=True,
                     output_attentions=False, return_dict=True, use_cache=False)
+        print("Got first layer activation")
 
         # do not save post embedding layer activations
         for lix, activation in enumerate(out.hidden_states[1:]):
@@ -89,6 +92,7 @@ def get_layer_activations_hf(bs: int, tokenized_prompts: torch.Tensor,
 
 
 def process_activation_batch(batch_activations, step, batch_mask=None):
+    cur_batch_size = batch_activations.shape[0]
     last_ix = batch_activations.shape[1] - 1
     batch_mask = batch_mask.to(int)
     last_entity_token = last_ix - torch.argmax(batch_mask.flip(dims=[1]), dim=1)
@@ -116,8 +120,9 @@ def quantize(activation_tensor: torch.Tensor) -> torch.Tensor:
 
 
 def write_activations_to_disk(layer_activations: dict[int, torch.Tensor], save_dir: str):
+    os.makedirs(save_dir, exist_ok=True)
     for layer_ix, activations in layer_activations.items():
-        save_path = os.path.join(save_dir, f"{layer_ix}.pt")
+        save_path = os.path.join(save_dir, f"{layer_ix:02}.pt")
         activations = quantize(activations.to(torch.float32))
         torch.save(activations, save_path)
 
@@ -126,9 +131,11 @@ def main(batch_size: int, save_dir: str):
     prompts, _labels = get_mixed_prompt_dataset(size=50000, prompt_type="is_a_kind_of", rng_seed=42)
     tokenized_prompts, attention_mask = tokenize_prompts(prompts)
 
+    print("Generated the dataset, computing layer activations")
     layer_activations = get_layer_activations_hf(batch_size, tokenized_prompts, attention_mask)
+    print("Got layer activations, saving them to disk")
     write_activations_to_disk(layer_activations, save_dir)
 
 
 if __name__ == "__main__":
-    main(batch_size=48, save_dir="hyper_hypo_activations0/")
+    main(batch_size=48, save_dir="hyper_hypo_activations1/")
